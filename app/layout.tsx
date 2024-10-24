29d3c0993497373455077bbb311f6a017707c536
Het Gajera <het36100@gmail.com>
2024-10-24 22:22:47 +0530

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,6 @@ export default function RootLayout({
           sizes="48x48"
         />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
--- components/UI/back.tsx
+++ components/UI/back.tsx
@@ -1,15 +1,34 @@
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, Download } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ThemeSwitcher from "./theme-switcher";
 
 export default function Backbutton() {
+  const [isResumePage, setIsResumePage] = useState(false);
+
+  useEffect(() => {
+    const handlePathChange = () => {
+      setIsResumePage(window.location.pathname === "/resume");
+    };
+    handlePathChange();
+    window.addEventListener("popstate", handlePathChange);
+    return () => {
+      window.removeEventListener("popstate", handlePathChange);
+    };
+  }, []);
   return (
     <header className="p-4 flex-shrink-0 flex justify-between bg-transparent backdrop-blur-md">
       <Link href="/">
         <ChevronLeft className="w-8 h-8 hover:text-mixed" />
       </Link>
-      <ThemeSwitcher />
+      <div className="flex items-center justify-center gap-8">
+        {isResumePage && (
+          <a href="/pdfs/Myres.pdf" download>
+            <Download />
+          </a>
+        )}
+        <ThemeSwitcher />
+      </div>
     </header>
   );
 }
