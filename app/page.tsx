2e35db5b9e48217a67598be15f61622ec842fa9a
Het Gajera <het36100@gmail.com>
2024-10-27 15:34:19 +0530

    minor update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justify-between">
+    <div className="min-h-screen flex flex-col justify-between overflow-hidden">
       <Header />
       <Navigation />
       <Footer />
